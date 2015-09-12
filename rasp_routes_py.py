71062e81b34d4f25eaf52cf0be6a21f5963380b5
GerardWassink <GerardWassink@users.noreply.github.com>
2015-09-12 15:57:49 +0200

    cleaned up code, GPIO input now working

--- rasp_routes_py.py
+++ rasp_routes_py.py
@@ -36,8 +36,13 @@
 # ------------------------------------------------------------------------
 
 from Adafruit_PWM_Servo_Driver import PWM
+import RPi.GPIO as GPIO
 import time
 import re
+
+								# set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+
 
 # ------------------------------------------------------------------------
 # Global variables and default values
@@ -68,43 +73,49 @@ class turnout:
 		self.posclos = x4
 		self.posthro = x5
 		self.name = x6
-
+								# Below some theoretical entries for future
+								# use, setting individual object attributes
 	def setid(self, who):
 		self.id = who
-
 	def setboard(self, who):
 		self.board = who
-
 	def setchannel(self, who):
 		self.channel = who
-
 	def setname(self, who):
 		self.name = who
-	
 	def setposclos(self, who):
 		self.posclos = who
-	
 	def setposthro(self, who):
 		self.posthro = who
-	
+
+
+								# Set the pulse-width value for this
+								# turnout, corresponding with it's 
+								# closed position
 	def setclosed(self):
 		global board, pwm, freq
-		if self.board != board:
 								# When this turnout board differs from
 								# previous, than change working board
-			board = self.board
-			pwm = PWM(self.board)	
-			pwm.setPWMFreq(freq)
+		if self.board != board:
+			board = self.board		# remember used board
+			pwm = PWM(self.board)	# set board to be used
+			pwm.setPWMFreq(freq)	# just to be sure, set frequency
+									# do it:
 		pwm.setPWM(self.channel, 0, self.posclos)
 	
+	
+								# Set the pulse-width value for this
+								# turnout, corresponding with it's 
+								# thrown position
 	def setthrown(self):
 		global board, pwm, freq
-		if self.board != board:
 								# When this turnout board differs from
 								# previous, than change working board
-			board = self.board
-			pwm = PWM(self.board)	
-			pwm.setPWMFreq(freq)
+		if self.board != board:
+			board = self.board		# remember used board
+			pwm = PWM(self.board)	# set board to be used
+			pwm.setPWMFreq(freq)	# just to be sure, set frequency
+									# do it:
 		pwm.setPWM(self.channel, 0, self.posthro)
 
 
@@ -117,19 +128,28 @@ class input:
 		self.id = x1
 		self.gpio = x2
 		self.name = x3
-		self.inpval = 1			# Default, assuming pull-up's
+		self.inpval = 0
 
+								# Below some theoretical entries for future
+								# use, setting individual object attributes
 	def setid(self, who):
 		self.id = who
-
 	def setgpio(self, who):
 		self.gpio = who
-
 	def setname(self, who):
 		self.name = who
-	
-	def getval(self, val):
-		self.inpval = 0			# QQQQQQQQQQQQQQ
+
+
+								# Setup this object's GPIO as an input line
+								# with pull-up
+	def setup(self):
+		GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+								# Read the current value os this object's
+								# GPIO line
+	def getval(self):
+		self.inpval = GPIO.input(self.gpio)
 
 
 # ------------------------------------------------------------------------
@@ -140,13 +160,12 @@ class route:
 		self.id = x1
 		self.routeid = x2
 		self.settings = x3
-
+								# Below some theoretical entries for future
+								# use, setting individual object attributes
 	def setid(self, who):
 		self.id = who
-
 	def setrouteid(self, who):
 		self.routeid = who
-
 	def setsettings(self, who):
 		self.settings = who
 
@@ -167,71 +186,73 @@ def read_config_file():
 	
 	myfile = open("rasp_routes_py.ini", "r")
 
-	regel = myfile.readline()	# read first line
+	line = myfile.readline()	# read first line
 	lc = 1						# set line count
 
-	while (regel != ""):		# read through file
-		regel = regel.rstrip()	# strip all space from right side
+	while (line != ""):		# read through file
+		line = line.rstrip()	# strip all spaces from right side
 
-		if (len(regel) > 0):	# empty line?
+		if (len(line) > 0):	# empty line?
 
 								# get first character to determine line type
-			m = re.match("(.).*", regel)
+			m = re.match("(.).*", line)
 			if m:
 				type = (m.group(1))
 		
 								# Is this a 'turnout' line?
-			if (type == "T" or type == "t"):
-				m = re.match("[Tt].*[:](.*)[:](.*)[:](.*)[:](.*)[:](.*)[:](.*)", regel)
-				if m:			# break down Turnout line and create new turnout
+								# break down Turnout line and create new turnout
 								# object in list of turnouts
+			if (type == "T" or type == "t"):
+				m = re.match("[Tt].*[:](.*)[:](.*)[:](.*)[:](.*)[:](.*)[:](.*)", line)
+				if m:
 					turnoutList.append(turnout(int(m.group(1)), \
 									int(m.group(2)), \
 									int(m.group(3)), \
 									int(m.group(4)), \
 									int(m.group(5)), \
 									m.group(6) ))
-									
 				else:
-					print "Syntax fout in Turnout regel", lc
+					print "Syntax error in Turnout line", lc
 
-									# Is this an 'input' line?
-			elif (type == "I" or type == "i"):
-				m = re.match("[Ii].*[:](.*)[:](.*)[:](.*)", regel)
-				if m:			# break down Input line and create new input
+								# Is this an 'input' line?
+								# break down Input line and create new input
 								# object in list of inputs
+			elif (type == "I" or type == "i"):
+				m = re.match("[Ii].*[:](.*)[:](.*)[:](.*)", line)
+				if m:
 					inputList.append(input(int(m.group(1)), \
 								int(m.group(2)), \
 								m.group(3)))
 				else:
-					print "Syntax fout in Input regel", lc
+					print "Syntax error in Input line", lc
 
-									# Is this a 'route' line?
-			elif (type == "R" or type == "r"):
-				m = re.match("[Rr].*[:](.*)[:](.*)[:](.*)", regel)
-				if m:			# break down Route line and create new route
+								# Is this a 'route' line?
+								# break down Route line and create new route
 								# object in list of routes
+			elif (type == "R" or type == "r"):
+				m = re.match("[Rr].*[:](.*)[:](.*)[:](.*)", line)
+				if m:
 					routeList.append(route(int(m.group(1)), \
 								m.group(2), \
 								m.group(3)))
 				else:			# line type not defined
-					print "Syntax fout in Route regel", lc
+					print "Syntax error in Route line", lc
 
-									# Is this a 'name' line?
+								# Is this a 'name' line?
+								# break down Name line
 			elif (type == "N" or type == "n"):
-				m = re.match("[Nn].*[:](.*)", regel)
-				if m:			# break down Name line
+				m = re.match("[Nn].*[:](.*)", line)
+				if m:
 					name = (m.group(1))
-					print "name =", name
 				else:
-					print "Syntax fout in Name regel", lc
+					print "Syntax error in Name line", lc
 
-									# Is this a 'name' line?
+									# Is this NOT a comment line?
 			elif (type != "#"):
-				print "Invalid line type in regel", lc
+				print "Invalid line type in line", lc
 
 								# read next line		
-		regel = myfile.readline()
+		line = myfile.readline()
 		lc = lc+1				# increment line count
 
 	myfile.close()				# close config file
@@ -239,33 +260,15 @@ def read_config_file():
 
 
 # ------------------------------------------------------------------------
-# test routine
+# show values from config file
 # ------------------------------------------------------------------------
-def setThemMin(x, y):
-	for i in range(x, y):
-		turnoutList[i].setclosed()
-		time.sleep(speed)
-
-
-# ------------------------------------------------------------------------
-# test routine
-# ------------------------------------------------------------------------
-def setThemMax(x, y):
-	for i in range(x, y):
-		turnoutList[i].setthrown()
-		time.sleep(speed)
-
-
-# ------------------------------------------------------------------------
-# main line
-# ------------------------------------------------------------------------
-if (read_config_file()):
-	print "Conf file read OKAY"
-
+def report_config_file():
 	print ""
 	print "# --- Turnout list ---"
 	for t in turnoutList:
-		print "id=", t.id, "board=", t.board, "channel=", t.channel, "posclos=", t.posclos, "posthro=", t.posthro, "name=", t.name
+		print "id=", t.id, "board=", t.board, \
+		"channel=", t.channel, "posclos=", t.posclos, \
+		"posthro=", t.posthro, "name=", t.name
 
 	print ""
 	print "# --- Input list ---"
@@ -277,10 +280,38 @@ if (read_config_file()):
 	for t in routeList:
 		print "id=", t.id, "routeid=", t.routeid, "settings=", t.settings
 
-	while (True):					# Change angle of servo 
-		setThemMin(0, 4)
-		setThemMax(0, 4)
+	return 0
 
+
+# ------------------------------------------------------------------------
+# main line
+# ------------------------------------------------------------------------
+if (read_config_file()):
+
+								# Initialize all inputs read
+	for i in inputList: i.setup()
+
+	report_config_file()
+
+	while (1):
+		for i in inputList: i.getval()
+		
+		for i in range(0, 4):
+			turnoutList[i].setclosed()
+			time.sleep(0.2)
+			
+			turnoutList[i].setthrown()
+			time.sleep(0.2)
+		
+		if inputList[19].inpval == 0:
+			print "1 pressed"
+		
+		if inputList[20].inpval == 0:
+			print "2 pressed"
+			break
+
+	GPIO.cleanup()
+	print "bye now"
 
 else:
-	print "Conf file read WRONG"
+	print "Error reading configuration file"
